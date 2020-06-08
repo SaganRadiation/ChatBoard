ad6f16be4fdd47fa50e5986890a01faebe8ff701
saganradiation <jakegrimes9@gmail.com>
2020-06-07 22:49:50 -0400

    idk

--- chat.py
+++ chat.py
@@ -5,29 +5,16 @@ import gevent
 from flask import Flask, render_template
 from flask_sockets import Sockets
 
-REDIS_URL = os.environ['REDIS_URL']
-REDIS_CHAN = 'chat'
-
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
 
 sockets = Sockets(app)
-redis = redis.from_url(REDIS_URL)
 
 class ChatBackend(object):
   """Interface for registering and updating WebSocket clients."""
 
   def __init__(self):
     self.clients = list()
-    self.pubsub = redis.pubsub()
-    self.pubsub.subscribe(REDIS_CHAN)
-
-  def __iter_data_(self):
-    for message in self.pubsub.listen():
-      data = message.get('data')
-      if message['type'] == 'message':
-        app.logger.info(u'Sending message: {}'.format(data))
-        yield data
 
   def register(self, client):
     self.clients.append(client)
@@ -38,16 +25,11 @@ class ChatBackend(object):
     except Exception:
       self.clients.remove(client)
 
-  def run(self):
-    for data in self.__iter_data():
-      for client in self.clients:
-        gevent.spawn(self.send, client, data)
-
-  def start(self):
-    gevent.spawn(self.run)
+  def send_to_all_clients(self, data):
+    for client in self.clients:
+      gevent.spawn(self.send, client, data)
 
 chats=ChatBackend()
-chats.start()
 
 @app.route('/')
 def hello():
@@ -58,10 +40,12 @@ def inbox(ws):
   while not ws.closed:
     gevent.sleep(0.1)
     message = ws.receive()
-
     if message:
-      app.logger.info(u'Inserting message: {}'.format(message))
-      redis.publish(REDIS_CHAN, message)
+      app.logger.info(u'Got message: {}'.format(message))
+      data = message.get('data')
+      if message['type'] == 'message':
+        app.logger.info(u'Broadcasting data: {}'.format(data))
+        chats.send_to_all_clients(data)
 
 @sockets.route('/receive/')
 def outbox(ws):
